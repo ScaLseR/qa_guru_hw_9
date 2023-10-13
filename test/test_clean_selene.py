19fd1a32ea77f43ece1b719482ea89143633b19a
a.skorik <a.skorik@petrovich.ru>
2023-10-13 17:44:04 +0300

    edited requirements.txt, test_clean_selene.py

--- test/test_clean_selene.py
+++ test/test_clean_selene.py
@@ -1,9 +1,10 @@
 """1. Чистый Selene (без шагов)"""
-from selene import browser, have, be, by
+from selene import have, be, by
+from selene.support.shared import browser
 
 
 def test_check_issue_name_only_selene():
-    browser.open('http://github.com')
+    browser.open('https://github.com/')
 
     browser.element('.header-search-button').click()
     browser.element('#query-builder-test').type('eroshenkoam/allure-example').press_enter()
